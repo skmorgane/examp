7fca00152106dfefe204008686b6863ff0d4155d
Ethan White <ethan@weecology.org>
2015-10-27 10:25:20 -0400

    Add function to determine size class

--- ts-analysis-size-comparison.R
+++ ts-analysis-size-comparison.R
@@ -2,3 +2,13 @@ get_data <- function(){
   data <- read.csv("surveys.csv")
   return(data)
 }
+
+get_data <- function(weight){
+  if (weight > 50){
+    size_class = 'large'
+  }
+  else {
+    size_class = 'small'
+  }
+  return(size_class)
+}
